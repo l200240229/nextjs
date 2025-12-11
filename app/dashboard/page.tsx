b030420f33ae3d0c6a7deb142dfc6db8e8575877
l200240229 <l200240229@student.ums.ac.id>
2025-12-11 23:35:48 +0700

    menghapus

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = "force-dynamic";
-
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
